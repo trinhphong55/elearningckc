876b2f6cb4796901a9e5f3fe3c995444efd33a8f
Yasuo <contraurung8426@gmail.com>
2020-07-17 23:36:23 +0700

    update chuoi ket noi mongodb

--- projects/backend/server.js
+++ projects/backend/server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
 
-const MONGODB_URI = 'mongodb://localhost:27017/'
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+const MONGODB_URI = 'mongodb://elearning_team:123@103.92.26.177:27017/testAngularckc'; //'mongodb://localhost:27017/'
 
 // Connect with MongoDB
 mongoose.connect(MONGODB_URI, {
@@ -12,23 +20,27 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
   useCreateIndex: true,
   useUnifiedTopology: true
-})
+});
 
 mongoose.connection.on('error', (err) => {
   console.log('Mongoose conection error:' + err)
-})
+});
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connected!')
-})
+});
 
 // Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(4100, function () {
-  console.log('Backend API running!')
-})
+  console.log('Backend API running!');
+});
 
-const apiRoutes = require('./api/api')
-app.use('/api', apiRoutes)
+app.use('/api', require('./api/api'));
+
+app.get('/', (req, res) => {
+  res.send('Back end API');
+});
