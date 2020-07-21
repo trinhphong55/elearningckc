72f96b114038f22c53a2e90610a2a14c078a0d12
Yasuo <contraurung8426@gmail.com>
2020-07-21 12:48:03 +0700

    Merge branch 'elearning' of https://github.com/CaoThangEducation/CaoThangAngular into elearning

--- projects/backend/api/api.js
+++ projects/backend/api/api.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const LopHocPhanRoutes = require('./LopHocPhan');
-const MonHoc = require('./MonHoc');
-
-router.use('/', LopHocPhanRoutes);
-router.use('/', MonHoc);
-
-=======
 const router = require('express').Router()
 const LopHocPhanRoutes = require('./LopHocPhan')
 const GiaoVienRoutes = require('./GiaoVien')
@@ -40,5 +30,4 @@ router.put("/khoabomon/:id", khoabomonController.updateKhoaBoMon);
 //Routes LoaiDonVi
 router.get("/loaidonvi", loaidonviController.getLoaiDonVi);
 
->>>>>>> 6f1639fbea27453ce5e4f7784cd6f68b3ae8dc79
 module.exports = router;
