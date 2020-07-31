5e5b08e854593f1309dceae5fd602f3356d91d82
hieule0207 <43146348+hieule0207@users.noreply.github.com>
2020-07-31 15:14:35 +0700

    loading tienichsv

---
+++ projects/cntt/src/app/components/tienichsv/tienichsv.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {TienIchSinhVienCnttService} from '../../services/TienIchSV.service'
+@Component({
+  selector: 'app-tienichsv',
+  templateUrl: './tienichsv.component.html',
+  styleUrls: ['./tienichsv.component.css']
+})
+export class TienichsvComponent implements OnInit {
+  TienIch: any = [];
+  constructor(private tienIchSinhVienCnttService: TienIchSinhVienCnttService) {
+    this.loadDanhSachTienIch();
+   }
+
+  ngOnInit(): void {
+  }
+  loadDanhSachTienIch() {
+    this.tienIchSinhVienCnttService.danhSachTienIch().subscribe((data) => {
+      console.log(data)
+      this.TienIch = data.data;
+    });
+  }
+}
