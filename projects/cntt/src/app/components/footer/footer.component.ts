b9e95d184700f50242234748ed792aa579bc8a30
Loc Thanh <locnt19@gmail.com>
2020-08-02 19:31:40 +0700

    Load thông tin chung lên trang chủ CNTT

--- projects/cntt/src/app/components/footer/footer.component.ts
+++ projects/cntt/src/app/components/footer/footer.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  @Input() footer: any;
+  ngOnInit(): void {}
 }
