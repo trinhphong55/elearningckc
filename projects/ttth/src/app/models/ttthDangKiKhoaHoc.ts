18a25ef94d55157d39d3d5ad12644676a6dac544
hieu159vnvn <hieu159vnvn@gmail.com>
2020-08-04 00:59:22 +0700

    dang ki khoa hoc TTTH

---
+++ projects/ttth/src/app/models/ttthDangKiKhoaHoc.ts
@@ -0,0 +1,11 @@
+export class ttthDangKiKhoaHoc {
+  mssv: String
+  hoten: String
+  ngaysinh: String
+  noisinh: String
+  sodienthoai: String
+  lophoc: String
+  hinhthuchoc: String
+  trangthai: Boolean
+  created_at: Date
+}
