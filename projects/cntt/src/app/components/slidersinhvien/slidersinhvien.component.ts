d7324bb20f9f55d01d13291b169d666dd22f5bad
Loc Thanh <locnt19@gmail.com>
2020-08-06 05:16:56 +0700

    Fix swiper

--- projects/cntt/src/app/components/slidersinhvien/slidersinhvien.component.ts
+++ projects/cntt/src/app/components/slidersinhvien/slidersinhvien.component.ts
@@ -1,23 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { CnttBoSuuTapService } from '../../services/bosuutap.service';
+declare var Swiper: any;
 
 @Component({
   selector: 'app-slidersinhvien',
   templateUrl: './slidersinhvien.component.html',
   styleUrls: ['./slidersinhvien.component.css'],
 })
-export class SlidersinhvienComponent implements OnInit {
+export class SlidersinhvienComponent implements OnInit, AfterViewInit {
   Item: any = [];
 
-  constructor(private cnttBoSuuTapService: CnttBoSuuTapService) {
+  constructor(private cnttBoSuuTapService: CnttBoSuuTapService) {}
+
+  ngOnInit(): void {
     this.loadDanhSachItemSlider();
   }
 
-  ngOnInit(): void {}
+  _slider: any;
+
+  ngAfterViewInit(): void {
+    this._slider = new Swiper('.gallery_home__slider', {
+      spaceBetween: 30,
+      loop: true,
+      centeredSlides: true,
+      lazy: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      speed: 2000,
+      slidesPerView: 1,
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+      },
+      pagination: {
+        el: '.gallery_home__slider__pagination',
+        clickable: true,
+      },
+    });
+  }
+
   loadDanhSachItemSlider() {
     this.cnttBoSuuTapService.danhSachItemSlider().subscribe((data) => {
-      // console.log(data);
       this.Item = data.data;
+      setTimeout(() => {
+        console.log('run time out');
+        this._slider.update();
+      }, 0);
     });
   }
 }
