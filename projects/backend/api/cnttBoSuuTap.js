221c2aff9c0c0ea54b41e2757c74be3d8d590c28
hieule0207 <43146348+hieule0207@users.noreply.github.com>
2020-08-08 23:25:34 +0700

    bst

--- projects/backend/api/cnttBoSuuTap.js
+++ projects/backend/api/cnttBoSuuTap.js
@@ -1,60 +1,87 @@
 const router = require("express").Router();
+const path = require("path");
 const BoSuuTap = require("../models/cnttBoSuuTap.model");
 const multer = require("multer");
-const PATH = "./uploads/cntt";
 
-let Storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, PATH);
+const FILE_PATH = "uploads/cntt/";
+
+// define multer storage configuration
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, FILE_PATH);
     },
-    filename: (req, file, callback) => {
-        let math = ["image/png", "image/jpeg"];
-        if (math.indexOf(file.mimetype) === -1) {
-            let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png.`;
-            return callback(errorMess, null);
-        }
-        let filename = `${file.originalname}`;
-        callback(null, filename);
+    filename: (req, file, cb) => {
+        const filename =
+            file.fieldname + "_" + Date.now() + path.extname(file.originalname);
+        cb(null, filename);
     },
 });
-var upload = multer({
-    storage: Storage,
-});
 
-router.post("/uploads", upload.single("image"), function (req, res) {
-    if (!req.file) {
-        console.log("No file is available!");
-        return res.send({
-            success: false,
-        });
-    }
-    console.log("filename:  " + req.file.filename);
-    console.log("File is available!");
-    res.send({
-        success: true,
-    });
-});
-
-router.post("/taobst", (req, res) => {
-    console.log("req.body.src  " + req.body.src);
-    var imgName = req.body.src.slice(12);
-    console.log(req.body)
-    var boSuuTap = new BoSuuTap({
-        maBST: req.body.maBST,
-        url: req.body.url,
-        alt: req.body.alt,
-        src: "uploads/cntt/" + imgName,
-        trangThai: 1
-    });
-    console.log(boSuuTap);
-    boSuuTap.save((err, data) => {
-        if (err) {
-            return next(err);
+const upload = multer({
+    storage: storage,
+    limits: {
+        files: 10, // allow up to 10 files per request
+        fieldSize: 5 * 1024 * 1024, // 5MB (max file size)
+    },
+    fileFilter: (req, file, cb) => {
+        // allow images only
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+            return cb(new Error("Only image are allowed."), false);
         }
-        res.json(data);
-    });
+        cb(null, true);
+    },
 });
+
 //#endregion
+function uploadPhotos(req, res, next) {
+    upload.single("image")(req, res, function (error) {
+        try {
+            console.log("run uploadPhotos");
+            const photos = req.file;
+            console.log(photos);
+            console.log(req.file);
+            // check if photos are available
+            if (!photos) {
+                res.status(400).json({
+                    status: false,
+                    message: "No photo is selected.",
+                });
+            } else {
+                req.body.uploads = photos;
+                next();
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+}
+//#endregion
+
+async function saveInformationsSlideShowToDatabase(req, res) {
+    try {
+        console.log("run save infomartion");
+        console.log(req.body);
+        console.log(req.body)
+        var boSuuTap = new BoSuuTap({
+            maBST: req.body.maBST,
+            url: req.body.url,
+            alt: req.body.alt,
+            src: "uploads/cntt/" + req.body.uploads.originalname,
+            trangThai: 1
+        });
+        console.log(boSuuTap);
+        boSuuTap.save((err, data) => {
+            if (err) {
+                return next(err);
+            }
+            res.json(data);
+        });
+    } catch (error) {
+        res.json({ message: "Thêm thất bại", error: error });
+    }
+}
+
+router.post("/taobst", uploadPhotos, saveInformationsSlideShowToDatabase);
 
 router.post("/chinhsuabst", async (req, res) => {
     console.log(" Chinh sua BST");
