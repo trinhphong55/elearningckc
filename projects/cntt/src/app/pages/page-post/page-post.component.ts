c59c5501d989ee9f1a9a67a2ede93e6a5464c388
hieule0207 <43146348+hieule0207@users.noreply.github.com>
2020-08-10 14:25:10 +0700

    post page

--- projects/cntt/src/app/pages/page-post/page-post.component.ts
+++ projects/cntt/src/app/pages/page-post/page-post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TinTucCnttService } from '../../services/tintuc.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as moment from 'moment';
+import { TienIchSinhVienCnttService } from '../../services/TienIchSV.service';
 @Component({
   selector: 'app-page-post',
   templateUrl: './page-post.component.html',
@@ -10,11 +11,15 @@ import * as moment from 'moment';
 export class PagePostComponent implements OnInit {
   cnttTinTuc: any = [];
   cnttTinTucKhac: any = [];
+  TienIch: any =[]
   constructor(
     private tinTucCnttService: TinTucCnttService,
     private actRoute: ActivatedRoute,
+    private tienIchSinhVienCnttService : TienIchSinhVienCnttService,
     private router: Router
-  ) {}
+  ) {
+    this.loadDanhSachTienIch();
+  }
 
   ngOnInit(): void {
     let id = this.actRoute.snapshot.paramMap.get('id');
@@ -35,5 +40,11 @@ export class PagePostComponent implements OnInit {
   formatDatetime(time: string): string {
     time = moment(time).format('HH:mm, DD-MM-YYYY');
     return time;
+  }
+  loadDanhSachTienIch() {
+    this.tienIchSinhVienCnttService.danhSachTienIch().subscribe((data) => {
+      // console.log(data);
+      this.TienIch = data.data;
+    });
   }
 }
