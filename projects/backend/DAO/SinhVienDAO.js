07729464d8c5b76336c31b20e485255ffee1bb37
Tot Pham <phamvantot1999@gmail.com>
2020-08-17 15:28:14 +0700

    fix api reset password, change password

--- projects/backend/DAO/SinhVienDAO.js
+++ projects/backend/DAO/SinhVienDAO.js
@@ -1,6 +1,18 @@
 const MongoDB = require('../MongoDB');
 const JWT = require('jsonwebtoken');
 const md5 = require('md5');
+const { check } = require('express-validator');
+const nodemailer = require('nodemailer');
+const { token } = require('morgan');
+const option = {
+  service: 'gmail',
+  auth: {
+      user: '0306171413@caothang.edu.vn',
+      pass: '301688325'
+  }
+};
+
+var transporter = nodemailer.createTransport(option);
 class SinhVienDAO extends MongoDB{
     constructor(){
       super();
@@ -30,6 +42,69 @@ class SinhVienDAO extends MongoDB{
       else {
         return false;
       }
+    }
+
+    async changePassword(mssv, oldPass, newPass, newPassConfirm){
+      let result = false;
+      const options = {
+        email: `${mssv}@caothang.edu.vn`,
+        matKhau: md5(oldPass)
+      }
+      const checkAccount = await this.find(options);
+      if(checkAccount.length > 0 && newPass == newPassConfirm){
+        await this.connectDB();
+        await this.conDb.collection(this.collectionName).updateOne({email: options.email}, {$set: {"matKhau": md5(newPass)}});
+        await this.closeDB();
+        result = true;
+      }
+      return result;
+    }
+
+    async getEmail(email){
+      const list = await this.find({
+        email,
+        trangThai: 1
+      });
+      return list;
+    }
+
+    async resetPassword(email){
+      const checkUser = await this.getEmail(email);
+      console.log('checkUser', checkUser);
+      let token;
+      if (checkUser && checkUser.length){
+        const temp = parseInt(Math.random() * 10000);
+        const obj = { id: email, temp};
+        token = JWT.sign(obj, '22222');
+        transporter.verify(function(error, success) {
+          if (error) {
+              console.log('error: ', error);
+          } else {
+              var mail = {
+                  from: '0306171413@caothang.edu.vn',
+                  to: email,
+                  subject: 'Link reset password',
+                  html: `<b> Xin chào </b>, <br/>
+                  Theo yêu cầu của bạn, chúng tôi gửi lại bạn thông tin mật mã tài khoản. <br/>
+                  <b>Password</b>: ${token.slice(160, 167)} </br>
+                  `
+              };
+              transporter.sendMail(mail, async function(error, info) {
+                  if (error) {
+                    console.log('error: ', error);
+                  } else {
+                    const sinhVienDAO = new SinhVienDAO();
+                    await sinhVienDAO.connectDB();
+                    await sinhVienDAO.conDb.collection(sinhVienDAO.collectionName).updateOne({email: mail.to}, {$set: {"matKhau": md5(token.slice(160, 167))}});
+                    await sinhVienDAO.closeDB();
+                    return true;
+                  }
+              });
+          }
+      });
+      }
+      if(token) return true;
+      return false;
     }
 }
 module.exports = SinhVienDAO;
