ce5db843e5e3e088ed539b1959e2b464b5c66649
NguyenHoangLinh-CDTH17PMC <soumaakito1478@gmail.com>
2020-08-20 21:36:48 +0700

    commit 09

--- projects/backend/MongoDB.js
+++ projects/backend/MongoDB.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 // const url = 'mongodb://localhost:27017/?readPreference=primary&authSource=GosuReport&appname=MongoDB%20Compass&ssl=false';
-const url = 'mongodb://elearning_team:123@103.92.26.177:27017/testAngularckc?retryWrites=true&w=majority?authSource=admin';
+const url = 'mongodb://ai_noi_mongo_die:123@103.92.26.177:27017/test?authSource=devAngular&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 class MongoDB {
   constructor() {
     this.conDb = null;
@@ -8,7 +8,7 @@ class MongoDB {
   }
 
   async connectDB() {
-    let dbName = 'testAngularckc';
+    let dbName = 'devAngular';
     try {
       if(!this.dbClose) {
         const connectRs = await MongoClient.connect(url, { useUnifiedTopology: true });
